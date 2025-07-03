cbcb4e3ce57917509824ce14797b38f206ffc25f
Daniel Palma Garcia <danpalma94@gmail.com>
2025-07-03 10:26:15 -0600

    quick fix for password

--- mongodb/testing/test.py
+++ mongodb/testing/test.py
@@ -6,5 +6,15 @@ load_dotenv(find_dotenv())
 
 password = os.environ.get("MONGODB_PWD")
 
-uri = f"mongodb+srv://danpg94:<{password}>@cherry-cluster.n0x2vso.mongodb.net/?retryWrites=true&w=majority&appName=Cherry-Cluster"
-client = MongoClient(uri)
+uri = f"mongodb+srv://danpg94:{password}@cherry-cluster.n0x2vso.mongodb.net/?retryWrites=true&w=majority&appName=Cherry-Cluster"
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+dbs = client.list_database_names()
+print(dbs)
