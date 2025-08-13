780c286ed330f6e7ec2aba4389488011d22c8d7d
Daniel Palma Garcia <danpalma94@gmail.com>
2025-08-13 01:06:38 -0600

    adding simple REST server to esp8266

--- esp8266/ver_2/main.py
+++ esp8266/ver_2/main.py
@@ -33,8 +33,33 @@ def do_connect():
             print('Error connecting: retrying in 10 s')
             utime.sleep(10)
 
-do_connect()
+def setup_socket():
+    # Create a socket and bind it to port 80
+    addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+    s = socket.socket()
+    s.bind(addr)
+    s.listen(3) # Max 3 pending connections
+    print('Listening on', addr)
+    return s
 
-while (1):
-    print("Connected")
-    utime.sleep(10)
+def handle_request(client_socket):
+    request = client_socket.recv(1024).decode()
+    print('Request:', request)
+
+    # Simple routing based on URL path
+    if 'GET /data' in request:
+        response = 'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nSending Data... [done]\n'
+    else:
+        response = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\nNot Found\n'
+    print(response)
+    client_socket.send(response.encode())
+    client_socket.close()
+
+
+do_connect()
+listening_socket = setup_socket()
+
+while True:
+    conn, addr = listening_socket.accept()
+    print('Got a connection from %s:%d' % (addr[0], addr[1]))
+    handle_request(conn)
